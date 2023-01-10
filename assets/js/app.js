9fec8d3b89987fb8eb2a1f65193d448601166638
Kevinco7 <kevindominguezansorena@gmail.com>
2023-01-10 11:53:28 -0300

    Primer Commit

---
+++ assets/js/app.js
@@ -0,0 +1,69 @@
+const botonUsd = document.getElementById('usd');
+const botonChl = document.getElementById('chl');
+const impuestoPais = 0.30;
+const retencion = 0.45;
+const salir = "ESC";
+let total =0;
+let totalRet = 0;
+let totalImpPais = 0;
+let valorDia = 0;
+let cantidadProductos=0;
+let valorProducto = 0;
+let numero_producto = 0;
+
+function convertirUsd(){
+
+    valorDia = Number(prompt("Ingrese el valor del dolar oficial Banco Nacion"));
+    while(valorDia ==""){
+    alert("Ingrese un valor");
+    valorDia = Number(prompt("Ingrese el valor del dolar oficial Banco Nacion"));
+    }
+    
+    do{
+        cantidadProductos = Number(prompt("Ingrese la cantidad de productos que compraste"));
+        if(cantidadProductos <=0){
+          alert("Ingrese una cantidad valida");
+        }
+    }while(cantidadProductos <=0);
+
+    for (let i = 1; i <= cantidadProductos; i++) {
+        
+        let nombre = prompt("Ingrese el nombre producto o ESC para salir");
+        if(escondicionSalida(nombre)){
+            alert("MUCHAS GRACIAS!")
+            break
+        }
+        numero_producto = i;
+        valorProducto = prompt("Ingrese el precio en u$d de "+ nombre );
+        while(valorProducto <= 0)
+        {
+          alert("Ingrese un precio valido");
+          valorProducto = prompt("Ingrese el precio en u$d de "+ nombre );
+        }
+        let totalConversion = valorProducto * valorDia;
+        let ret = totalConversion * retencion;
+        let imp = totalConversion * impuestoPais;
+        total = total + totalConversion;
+        totalRet = totalRet + ret;
+        totalImpPais = totalImpPais + imp;
+             
+        alert( `Producto ${numero_producto} con nombre ${nombre} y monto total $ ${totalConversion}.Impuesto PAIS $ ${imp} Percepcion Ganancias $ ${ret}`);
+        // console.log( `Producto ${numero_producto} con nombre ${nombre} y monto total $ ${totalConversion}.Impuesto PAIS $ ${imp} Percepcion Ganancias $ ${ret}`);
+    }
+    alert(`El valor total de tus ${numero_producto} compras es de: ${total}ARS \n Impuesto Pais total es  ${totalImpPais}ARS \n Percepcíon RG 4815 es ${totalRet}ARS`)
+    
+};
+
+
+function escondicionSalida(texto){
+    if(texto==salir){
+       return true
+    }
+    return false
+}
+
+
+
+botonUsd.onclick = convertirUsd;
+// botonChl.onclick = convertirCHL;
+
