b2caf4c37c2ab5bfdf478785728478cc3634cb16
Kevinco7 <kevindominguezansorena@gmail.com>
2023-02-10 16:59:41 -0300

    fix en los totales y cambios en  css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
 const valorImpuesto = 0.3;
 const valorRetencion = 0.45;
+const valorBienesPersonales = 0.25;
 const SALIR = "ESC";
 let productos = JSON.parse(localStorage.getItem("producto")) || [];
 // let productos = [];
-let totales = parseFloat(0);
-let totalSuma = parseFloat(0);
-
+let totalSuma = parseFloat(0).toFixed(2);
+let totalSI = parseFloat(0).toFixed(2);
+let bienesPersonales =  parseFloat(0).toFixed(2);
 class Producto {
   constructor(
     id,
@@ -18,11 +19,11 @@ class Producto {
   ) {
     this.id = id;
     this.descripcion = descripcion;
-    this.precio = parseFloat(precio);
-    this.precioConversion = parseFloat(precioConversion);
-    this.retencion = parseFloat(retencion);
-    this.impuesto = parseFloat(impuesto);
-    this.totalImpuesto = parseFloat(totalImpuesto);
+    this.precio = parseFloat(precio).toFixed(2);
+    this.precioConversion = parseFloat(precioConversion).toFixed(2);
+    this.retencion = parseFloat(retencion).toFixed(2);
+    this.impuesto = parseFloat(impuesto).toFixed(2);
+    this.totalImpuesto = parseFloat(totalImpuesto).toFixed(2);
   }
   sumaTotal() {
     precioTotalProducto += this.precio;
@@ -81,7 +82,7 @@ function agregarProducto() {
     )
   );
   mostrarProductos();
-  formulario.reset();
+  formulario.reset(); 
   limpiarObjeto();
 }
 
@@ -98,7 +99,8 @@ function mostrarProductos() {
   //funcion para limpiar el listado y que no se repita con lo creado
   limpiarHTML();
   const divProductos = document.querySelector(".div-productos");
-
+  const $cuerpoTabla = document.querySelector("#cuerpoTabla");
+  const $cuerpoTotales = document.querySelector("#cuerpoTotales");
   productos.forEach((producto) => {
     const {
       id,
@@ -112,35 +114,104 @@ function mostrarProductos() {
 
     const parrafo = document.createElement("p");
     parrafo.innerHTML = `Descripción ${descripcion} u$d ${precio} $ ${precioConversion} ars 
-                         Retención: $ ${retencion}ars  impuesto: ${impuesto} ars total: $ ${totalImpuesto} `;
+                         Retención: $ ${retencion} ars impuesto: $ ${impuesto} ars total: $ ${totalImpuesto} ars `;
 
     parrafo.dataset.id = id;
-    totalSuma = calcularTotal(productos);
 
-    totales = document.querySelector("#total");
-    totales.innerHTML = totalSuma;
+    // totalSuma = calcularTotal(productos);
+    // totales = document.querySelector("#total");
+    // totales.innerHTML = totalSuma;
 
     //localstorage
     const productoJSON = JSON.stringify(productos);
     localStorage.setItem("producto", productoJSON);
 
-    const editarBoton = document.createElement("button");
-    editarBoton.onclick = () => cargarProducto(producto);
-    editarBoton.textContent = "Editar";
-    editarBoton.classList.add("btn", "btn-editar");
-    parrafo.append(editarBoton);
 
-    const eliminarBoton = document.createElement("button");
-    eliminarBoton.onclick = () => eliminarProducto(id);
-    eliminarBoton.textContent = "Eliminar";
-    eliminarBoton.classList.add("btn", "btn-eliminar");
-    parrafo.append(eliminarBoton);
 
-    const hr = document.createElement("hr");
+    
+    // const editarBoton = document.createElement("button");
+    // editarBoton.onclick = () => cargarProducto(producto);
+    // editarBoton.textContent = "Editar";
+    // editarBoton.classList.add("btn", "btn-editar");
+    // parrafo.append(editarBoton);
+    
+    // const eliminarBoton = document.createElement("button");
+    // eliminarBoton.onclick = () => eliminarProducto(id);
+    // eliminarBoton.textContent = "Eliminar";
+    // eliminarBoton.classList.add("btn", "btn-eliminar");
+    // parrafo.append(eliminarBoton);
+    
+    // const hr = document.createElement("hr");
+    
+    // divProductos.appendChild(parrafo);
+    // divProductos.appendChild(hr);
+    
+    //tabla de productos
+    const $tr = document.createElement("tr");    
+    let tdNombre = document.createElement("td");
+    tdNombre.textContent = producto.descripcion; 
+    $tr.appendChild(tdNombre);   
+    let tdPrecio = document.createElement("td");
+    tdPrecio.textContent =`u$d ${producto.precio}`;
+    $tr.appendChild(tdPrecio);    
+    let tdPrecioSI = document.createElement("td");
+    tdPrecioSI.textContent = `$ ${producto.precioConversion}`;
+    $tr.appendChild(tdPrecioSI); 
+    let tdRetencion = document.createElement("td");
+    tdRetencion.textContent = `$ ${producto.retencion}`;
+    $tr.appendChild(tdRetencion); 
+    let tdImpuestos = document.createElement("td");
+    tdImpuestos.textContent = `$ ${producto.impuesto}`;
+    $tr.appendChild(tdImpuestos); 
+    let tdPrecioCI = document.createElement("td");
+    tdPrecioCI.textContent = `$ ${producto.totalImpuesto}`;
+    $tr.appendChild(tdPrecioCI); 
+    
+    let tdEditar = document.createElement("button");
+    tdEditar.onclick = () => cargarProducto(producto);
+    tdEditar.textContent = "Editar";
+    tdEditar.classList.add("btn", "btn-editar");
+    $tr.appendChild(tdEditar); 
 
-    divProductos.appendChild(parrafo);
-    divProductos.appendChild(hr);
-  });
+    let tdEliminar = document.createElement("button");
+    tdEliminar.onclick = () => eliminarProducto(id);
+    tdEliminar.textContent = "Eliminar";
+    tdEliminar.classList.add("btn", "btn-eliminar");
+    $tr.appendChild(tdEliminar);
+    
+    $cuerpoTabla.appendChild($tr);
+    
+    //tabla de totales
+    Totales();
+    });
+  }
+  
+function Totales(){
+     totalSuma = calcularTotal(productos);
+     let totales = document.querySelector("#PrecioTotal");
+     totales.innerHTML = `u$d ${totalSuma}`;
+
+     totalSI = calcularTotalSinImpuesto(productos);
+     let totalesSI = document.querySelector("#TotalSinImpuestos");
+     totalesSI.innerHTML = `$ ${totalSI}`;
+
+     let impPais = parseFloat(calcularTotalImpuestoPais(productos)).toFixed(2);     
+     let totalImpPais = document.querySelector("#totalImpuestos");
+     totalImpPais.innerHTML = `$ ${impPais}`;
+     let ImpRet = parseFloat(calcularTotalRetencion(productos)).toFixed(2);
+     let totalRetenciones = document.querySelector("#totalRetenciones");
+     totalRetenciones.innerHTML = `$ ${ImpRet}`; 
+
+      let bienes = parseFloat(calcularImpuesto25(productos)).toFixed(2);
+      let totalBienesPersonales = document.querySelector("#totalBienes");
+      totalBienesPersonales.innerHTML = `$ ${bienes}`; 
+
+     let gastoTotal = parseFloat(calcularGastoTotal(productos)).toFixed(2);
+     let totalDeGasto = document.querySelector("#totalGasto");
+     totalDeGasto.innerHTML = `$ ${gastoTotal}`; 
+      
+
+     
 }
 
 function cargarProducto(producto) {
@@ -154,14 +225,13 @@ function cargarProducto(producto) {
   editando = true;
 }
 
-function editarProducto() {
+function editarProducto() {  
   Producto.descripcion = descripcionInput.value;
   Producto.precio = precioInput.value;
   Producto.precioConversion = precioInput.value * dolarOficial.value;
   Producto.retencion = Producto.precioConversion * valorRetencion;
   Producto.impuesto = Producto.precioConversion * valorImpuesto;
-  Producto.totalImpuesto =
-    Producto.precioConversion + Producto.retencion + Producto.impuesto;
+  Producto.totalImpuesto = Producto.precioConversion + Producto.retencion + Producto.impuesto;
 
   productos.map((producto) => {
     if (producto.id === Producto.id) {
@@ -179,13 +249,13 @@ function editarProducto() {
   mostrarProductos();
 
   formulario.reset();
+
   formulario.querySelector("button[type=submit]").textContent = "Agregar";
   editando = false;
 }
 
 function eliminarProducto(id) {
   productos = productos.filter((producto) => producto.id !== id);
-
   limpiarHTML();
   mostrarProductos();
 }
@@ -198,14 +268,79 @@ function calcularTotal(items) {
   return parseFloat(totalValor);
 }
 
+function calcularTotalSinImpuesto(items) {
+  let totalValorSI = 0;
+  for (const item of items) {
+    totalValorSI = parseFloat(totalValorSI) + parseFloat(item.precioConversion);
+  }
+  return parseFloat(totalValorSI);
+}
+
+function calcularTotalRetencion(items) {
+  let totalValorRetenciones = 0;
+  for (const item of items) {
+    totalValorRetenciones = parseFloat(totalValorRetenciones) + parseFloat(item.retencion);
+  }
+  return parseFloat(totalValorRetenciones);
+}
+
+function calcularTotalImpuestoPais(items) {
+  let totalValorimpuesto = 0;
+  for (const item of items) {
+    totalValorimpuesto = parseFloat(totalValorimpuesto) + parseFloat(item.impuesto);
+  }
+  return parseFloat(totalValorimpuesto);
+}
+
+
+function calcularImpuesto25(items) {
+  bienesPersonales = 0;
+  let total;
+  
+  for (const item of items) { 
+    total = parseFloat(calcularTotal(items));   
+    let totalSI = parseFloat(calcularTotalSinImpuesto(items))
+    if(total >= 300)
+    {
+      bienesPersonales = totalSI * valorBienesPersonales;
+      
+    } 
+  }
+  return parseFloat(bienesPersonales);
+}
+
+function calcularGastoTotal(items) {
+  let precioFinal = 0;
+  let bienesP;
+  for (const item of items) {
+     bienesP = parseFloat(calcularImpuesto25(items));
+    precioFinal += parseFloat(item.totalImpuesto);
+  }
+  precioFinal = precioFinal + parseFloat(bienesP);
+  return parseFloat(precioFinal);
+}
+
+
+
 function limpiarHTML() {
   //elementos hijos del divproductos
-  const divProductos = document.querySelector(".div-productos");
-
-  //mientras divProd tenga hijos,se eliminaran
-  while (divProductos.firstChild) {
-    divProductos.removeChild(divProductos.firstChild);
-
+  // const divProductos = document.querySelector(".div-productos");
+  const $cuerpoTabla = document.querySelector("#cuerpoTabla");
+  while ($cuerpoTabla.firstChild) {
+    $cuerpoTabla.removeChild($cuerpoTabla.firstChild);
     localStorage.clear();
+     Totales();
   }
+  
+  //mientras divProd tenga hijos,se eliminaran
+  // while (divProductos.firstChild) {
+  //   divProductos.removeChild(divProductos.firstChild);
+  //   localStorage.clear();
+  //   totalSuma = calcularTotal(productos);
+  //   totales.innerHTML = totalSuma;
+  // }
+
+
+
+
 }
